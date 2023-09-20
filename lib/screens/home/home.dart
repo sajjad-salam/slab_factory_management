a4dd99a90998d7109aef63f15024a7a473eb1038
sajjad-salam <sajjad.salam.teama@gmail.com>
2023-09-20 18:15:32 +0300

    Update the icons and colors of the buttons on the home screen

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -27,92 +27,76 @@ class _champ_screenState extends State<home_screen> {
       ),
       body: SafeArea(
         bottom: false,
-        child: Container(
+        child: SizedBox(
           height: size.height,
           child: Column(
             children: [
-              SizedBox(height: 10), // Adjust the spacing between the items
+              const SizedBox(
+                  height: 10), // Adjust the spacing between the items
 
               mybutton(
                 rout: "",
                 size: MediaQuery.of(context).size,
                 name: 'الصادر',
-                icon: Icon(Icons.send),
+                icon: const Icon(Icons.send),
                 color: Colors.lightBlueAccent,
               ),
 
-              SizedBox(height: 10), // Adjust the spacing between the items
+              const SizedBox(
+                  height: 10), // Adjust the spacing between the items
 
               mybutton(
                 rout: "",
                 size: MediaQuery.of(context).size,
                 name: 'الوارد',
-                icon: Icon(Icons.all_inbox_sharp),
+                icon: const Icon(Icons.all_inbox_sharp),
                 color: Colors.greenAccent,
               ),
 
-              SizedBox(height: 10), // Adjust the spacing between the items
+              const SizedBox(
+                  height: 10), // Adjust the spacing between the items
               mybutton(
                 rout: "",
                 size: MediaQuery.of(context).size,
                 name: 'الأنتاخ',
-                icon: Icon(Icons.check_box),
+                icon: const Icon(Icons.check_box),
                 color: Colors.purpleAccent,
               ),
 
               // Add more mybutton widgets with appropriate data and spacing
 
-              SizedBox(height: 10), // Adjust the spacing between the items
+              const SizedBox(
+                  height: 10), // Adjust the spacing between the items
 
               mybutton(
                 rout: "",
                 size: MediaQuery.of(context).size,
                 name: 'العمال',
-                icon: Icon(Icons.work),
+                icon: const Icon(Icons.work),
                 color: Colors.redAccent,
               ),
 
-              SizedBox(height: 10), // Adjust the spacing between the items
+              const SizedBox(
+                  height: 10), // Adjust the spacing between the items
 
               mybutton(
                 rout: "",
                 size: MediaQuery.of(context).size,
                 name: 'ملاحضات',
-                icon: Icon(Icons.notes),
+                icon: const Icon(Icons.notes),
                 color: Colors.amberAccent,
               ),
 
-              SizedBox(height: 10), // Adjust the spacing between the items
+              const SizedBox(
+                  height: 10), // Adjust the spacing between the items
 
               mybutton(
                 rout: "",
                 size: MediaQuery.of(context).size,
                 name: 'الجرد الشهري',
-                icon: Icon(Icons.library_books_outlined),
-                color: Color.fromARGB(255, 184, 138, 123),
+                icon: const Icon(Icons.library_books_outlined),
+                color: const Color.fromARGB(255, 184, 138, 123),
               ),
-              // Container(
-              //   height: size.height, // Adjust the height according to your needs
-              //   child: ListView.builder(
-              //     itemCount: 6,
-              //     itemBuilder: (BuildContext context, int index) {
-              //       // final buttonData = myButtonList[index];
-              //       return mybutton(
-              //         size: MediaQuery.of(context).size,
-              //         name: 'name',
-              //         icon: Icon(Icons.abc),
-              //         color: Colors.black,
-              //       );
-              //     },
-              //   ),
-              // ),
-
-              // ListView.builder(
-              //   itemCount: 6,
-              //   itemBuilder: (context, index) => mybutton(
-              //     size: size,
-              //   ),
-              // ),
             ],
           ),
         ),
