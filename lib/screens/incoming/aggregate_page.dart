ff2cf08e3ed26c5cc827e5bd32843a6db3a801d5
sajjad-salam <sajjad.salam.teama@gmail.com>
2023-10-18 14:06:13 +0300

    ....

--- lib/screens/incoming/aggregate_page.dart
+++ lib/screens/incoming/aggregate_page.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: file_names, avoid_print
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
